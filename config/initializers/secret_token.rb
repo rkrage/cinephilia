115bc9468591ec0cb153e16bef41424c6483dad8
ryan <krage.ryan@gmail.com>
2012-02-23 19:23:16 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cinephilia::Application.config.secret_token = 'a49f9b87f7157e3e81a58d32c2d19580e9a67c920e234f98b58939df310a6fa197765416557d512afc935793af313a6c0e4b25b45995198832d692a4efdc03ed'
