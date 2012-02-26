a3d636dc4c14c933b0cff4b17db6b98b2964656e
ryan <krage.ryan@gmail.com>
2012-02-26 17:01:18 -0600

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
-  helper :all
-  include SslRequirement unless (Rails.env.development? or
-  Rails.env.test?)
 end
