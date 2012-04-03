52e7cc48498bea75c003b572f44c8657da1d9065
ryan <krage.ryan@gmail.com>
2012-04-03 17:50:02 -0500

    small fix

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ end
 module Cinephilia
   class Application < Rails::Application
     config.force_ssl = false
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
