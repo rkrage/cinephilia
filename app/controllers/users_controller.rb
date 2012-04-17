5b8b8c0a5d2c71c1382c7ba8d6887d2c83a94845
ryan <krage.ryan@gmail.com>
2012-04-17 16:39:56 -0500

    fixed user suggestions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,22 +35,28 @@ class UsersController < ApplicationController
     @title = @user.name + " Like List"
     @movies = @user.movies.where(:likes => {:like_list => true}).paginate(:page => params[:page], :per_page => 20, :order => 'title')
   end
-  
+
   def movie_suggestions
     @user = User.find(params[:id])
     @title = "My Suggestions"
     genres = get_genres(@user)
     @first = genres.first
     @second = genres.second
-    alreadySeen = @user.movies
-    @movies = Movie.where("((genre1 = '#{@first}' OR genre2 = '#{@first}') AND user_rating >= '6.5') OR ((genre1 = '#{@second}' OR genre2 = '#{@second}') AND user_rating >= '7')").order("user_rating DESC")
-    followers = @user.followers
-    follower_movies = []
-    followers.each do |follower|
-       follower_movies = follower_movies + follower.movies.where("user_rating >= '5'")
+    if @second.nil?
+    @second = @first
     end
-    follower_movies = follower_movies.uniq.sort {|a,b| b.user_rating <=> a.user_rating}
-    @movies = (@movies + follower_movies - alreadySeen).uniq.paginate(:page => params[:page], :per_page => 20)
+    @movies = nil
+    if !@first.nil?
+      alreadySeen = @user.movies
+      @movies = Movie.where("((genre1 = '#{@first}' OR genre2 = '#{@first}') AND user_rating >= '6.5') OR ((genre1 = '#{@second}' OR genre2 = '#{@second}') AND user_rating >= '7')").order("user_rating DESC")
+      followers = @user.followers
+      follower_movies = []
+      followers.each do |follower|
+        follower_movies = follower_movies + follower.movies.where("user_rating >= '5'")
+      end
+      follower_movies = follower_movies.uniq.sort {|a,b| b.user_rating <=> a.user_rating}
+      @movies = (@movies + follower_movies - alreadySeen).uniq.paginate(:page => params[:page], :per_page => 20)
+    end
   end
 
   def user_suggestions
@@ -59,20 +65,31 @@ class UsersController < ApplicationController
     genres = get_genres(@user)
     @first = genres.first
     @second = genres.second
-    @users = []
-    temp_g1 = nil
-    temp_g2 = nil
-    temp_genres = nil
-    User.all.each do |temp_user|
-      temp_genres = get_genres(temp_user)
-      temp_g1 = temp_genres.first
-      temp_g2 = temp_genres.second
-      if temp_user != @user and (temp_g1 == @first or temp_g1 == @second or temp_g2 == @first or temp_g2 == @second)
-        @users << temp_user
-      end
+    if @second.nil?
+      @second = @first
     end
-    @users = (@users - @user.following).sort {|a,b| b.followers.length <=> a.followers.length}
-    @users = @users.paginate(:page => params[:page], :per_page => 10)
+    @users2 = []
+    if !@first.nil?
+      temp_g1 = nil
+      temp_g2 = nil
+      temp_genres = nil
+      User.all.each do |temp_user|
+        temp_genres = get_genres(temp_user)
+        temp_g1 = temp_genres.first
+        temp_g2 = temp_genres.second
+        if temp_g2.nil?
+          temp_g2 = temp_g1
+        end
+        if !temp_g1.nil?
+
+          if ((temp_user != @user) and ((temp_g1.to_s == @first.to_s) or (temp_g1.to_s == @second.to_s) or (temp_g2.to_s == @first.to_s) or (temp_g2.to_s == @second.to_s)))
+            @users2 << temp_user
+          end
+        end
+      end
+      @users2 = (@users2 - @user.following).sort {|a,b| b.followers.length <=> a.followers.length}
+      @users2 = @users2.paginate(:page => params[:page], :per_page => 10)
+    end
   end
 
   def results
@@ -131,11 +148,12 @@ class UsersController < ApplicationController
   private
 
   def get_genres(user)
-    genres1 = @user.movies.select("genre1").collect {|x| x.genre1}
-    genres2 = @user.movies.select("genre2").collect {|x| x.genre2}
+    genres1 = user.movies.select("genre1").collect {|x| x.genre1}.compact
+    genres2 = user.movies.select("genre2").collect {|x| x.genre2}.compact
     genres = genres1 + genres2
     freq = genres.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
     genres = genres.sort_by { |v| freq[v] }.reverse.uniq
+    return genres
   end
 
   def authenticate
@@ -145,7 +163,7 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(permission_path) unless current_user?(@user)
-    
+
   end
 
 end
